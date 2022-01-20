d582cc8ff7bb74e75488ef3f2160beec89471bd1
Ann Safo <asafo@unc.edu>
2022-01-19 22:19:15 -0500

    ex1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,7 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = 730478408
+
+word_selection: str = input("Enter a 5-character word: ")
+character_selection: str = input("Enter a single character: ")
+print("Searching for " + "character_selection " + "in " + "word_selection")

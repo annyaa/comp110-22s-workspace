cb10576236d1b52dc96b9185d036918c5ab35b30
Ann Safo <asafo@unc.edu>
2022-01-29 18:49:38 -0500

    progress on ex1

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,19 @@
+"""EX02 - Practing loops with Wordle."""
+
+__author__ = "730478408"
+
+secret: str = "python"
+
+guess: str = input("What is your 6-letter guess? ")
+
+while len(guess) != len(secret):
+    guess = input("That was not 6 letters! Try again: ")
+if guess == secret:
+    print("Woo! You got it!")
+else:
+    print("Not quite. Play again soon!")   
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+

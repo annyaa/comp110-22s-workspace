9272f664d8516cb84b8e517eae5880e703d5da3f
Ann Safo <asafo@unc.edu>
2022-03-01 23:53:18 -0500

    progress

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -6,14 +6,18 @@ __author__ = "730478408"
 def invert(values_to_invert: dict[str, str]) -> dict[str, str]:
     """Given a dictionary [str, str], returns a dictionary [str,str] that inverts the inputted keys and the values such that the keys of the input list become the values of the output list and vice versa."""
     dictionary_after_inversion: dict = {}
-    for key, value in values_to_invert:
-        dictionary_after_inversion[value] = key
+    for key, value in values_to_invert.items():
+        if value in dictionary_after_inversion:
+            raise KeyError(f"the key {dictionary_after_inversion[value]} has already been encountered. Associate this value with a new, unique key.")
+        else:
+            dictionary_after_inversion[value] = key
     return dictionary_after_inversion
-
+    
 
 def favorite_color(names_and_color_inputs: dict[str, str]) -> str:
     """Given that its only parameter is a dictionary [str, str] with names of people as keys and their corresponding favorite colors as values, returns a str which is the color that appears most frequently."""
-
+    output_string_value: str = ""
+    for key in 
 
 def count(counter_input_values: list[str]) -> dict[str, int]:
     """Given a list of strings as its input, returns a dict[str, int] where each key is a unique value in the given list and each value associated is the count of the number of times that value appeared in the input list."""
--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -3,4 +3,31 @@
 __author__ = "730478408"
 
 
-from dictionary import invert #  sub, concat
+from dictionary import invert
+import pytest
+
+
+"""Testing the invert function."""
+
+
+with pytest.raises(KeyError):
+    my_dictionary = {'kris': 'jordan', 'michael': 'jordan'}
+    invert(my_dictionary)
+    
+
+def test_invert_empty() -> None:
+    """Tests the situation where the input dictionary is an empty dictionary."""
+    values_to_invert: dict[str, str] = {}
+    assert invert(values_to_invert) == {}
+
+
+def test_invert_single_key_value_pair() -> None:
+    """Tests the situation where the input dictionary contains a single key-vale pair."""
+    values_to_invert: dict[str, str] = {'apple': 'cat'}
+    assert invert(values_to_invert) == {'cat': 'apple'}
+
+
+def test_invert_multiple_key_value_pairs() -> None:
+    """Tests the situation where the input dictionary contains multiple key-vale pairs."""
+    values_to_invert: dict[str, str] = {'a': 'z', 'b': 'y', 'c': 'x'}
+    assert invert(values_to_invert) == {'z': 'a', 'y': 'b', 'x': 'c'}

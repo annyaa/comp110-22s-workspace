7f82ebe8f0ffa825acd0647c1f9f0130c7170048
Ann Safo <asafo@unc.edu>
2022-03-02 18:51:46 -0500

    corection

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -5,7 +5,7 @@ __author__ = "730478408"
 
 def invert(values_to_invert: dict[str, str]) -> dict[str, str]:
     """Given a dictionary [str, str], returns a dictionary [str,str] that inverts the inputted keys and the values such that the keys of the input list become the values of the output list and vice versa."""
-    dictionary_after_inversion: dict = {}
+    dictionary_after_inversion: dict[str, str] = {}
     for key in values_to_invert:
         dictionary_after_inversion[values_to_invert[key]] = key
     if len(values_to_invert) > len(dictionary_after_inversion):
@@ -32,7 +32,7 @@ def favorite_color(names_and_color_inputs: dict[str, str]) -> str:
 
 def count(counter_input_values: list[str]) -> dict[str, int]:
     """Given a list of strings as its input, returns a dict[str, int] where each key is a unique value in the given list and each value associated is the count of the number of times that value appeared in the input list."""
-    counter_generated_dictionary: dict = {}
+    counter_generated_dictionary: dict[str, int] = {}
     for counting_variable in counter_input_values:
         if counting_variable in counter_generated_dictionary:
             counter_generated_dictionary[counting_variable] += 1

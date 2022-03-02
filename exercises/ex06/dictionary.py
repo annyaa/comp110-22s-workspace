c4c1463b569cdd25070348a483cbe52fb89c8b3e
Ann Safo <asafo@unc.edu>
2022-03-02 18:45:57 -0500

    finalchanges

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -6,18 +6,29 @@ __author__ = "730478408"
 def invert(values_to_invert: dict[str, str]) -> dict[str, str]:
     """Given a dictionary [str, str], returns a dictionary [str,str] that inverts the inputted keys and the values such that the keys of the input list become the values of the output list and vice versa."""
     dictionary_after_inversion: dict = {}
-    for key, value in values_to_invert.items():
-        if value in dictionary_after_inversion:
-            raise KeyError(f"the key {dictionary_after_inversion[value]} has already been encountered. Associate this value with a new, unique key.")
-        else:
-            dictionary_after_inversion[value] = key
+    for key in values_to_invert:
+        dictionary_after_inversion[values_to_invert[key]] = key
+    if len(values_to_invert) > len(dictionary_after_inversion):
+        raise KeyError("found duplicate keys.")
     return dictionary_after_inversion
-    
+
 
 def favorite_color(names_and_color_inputs: dict[str, str]) -> str:
     """Given that its only parameter is a dictionary [str, str] with names of people as keys and their corresponding favorite colors as values, returns a str which is the color that appears most frequently."""
-    output_string_value: str = ""
-    for key in 
+    color_frequency_dictionary: dict[str, int] = {}
+    for key in names_and_color_inputs:
+        if names_and_color_inputs[key] in color_frequency_dictionary:
+            color_frequency_dictionary[names_and_color_inputs[key]] += 1
+        else:
+            color_frequency_dictionary[names_and_color_inputs[key]] = 1
+    max: str = ""
+    default: int = 0
+    for key in color_frequency_dictionary:
+        if color_frequency_dictionary[key] > default:
+            max = key
+            default = color_frequency_dictionary[key] 
+    return max
+
 
 def count(counter_input_values: list[str]) -> dict[str, int]:
     """Given a list of strings as its input, returns a dict[str, int] where each key is a unique value in the given list and each value associated is the count of the number of times that value appeared in the input list."""
--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -3,7 +3,7 @@
 __author__ = "730478408"
 
 
-from dictionary import invert
+from dictionary import invert, count, favorite_color
 import pytest
 
 
@@ -31,3 +31,69 @@ def test_invert_multiple_key_value_pairs() -> None:
     """Tests the situation where the input dictionary contains multiple key-vale pairs."""
     values_to_invert: dict[str, str] = {'a': 'z', 'b': 'y', 'c': 'x'}
     assert invert(values_to_invert) == {'z': 'a', 'y': 'b', 'x': 'c'}
+
+
+"""Testing the invert function."""
+
+
+def test_count_empty() -> None:
+    """Tests the situation where the input list is an empty list."""
+    collection_to_count: list[str] = []
+    assert count(collection_to_count) == {}
+
+
+def test_count_single_item() -> None:
+    """Tests the situation where the input list contains just one item."""
+    collection_to_count: list[str] = ["Ann"]
+    assert count(collection_to_count) == {'Ann': 1}
+
+
+def test_count_many_strings_with_single_recurrence() -> None:
+    """Tests the situation where the input list contains many items but each item occurs only once."""
+    collection_to_count: list[str] = ["Ann", "Yaa", "Safo", "Nana", "Edwin"]
+    assert count(collection_to_count) == {'Ann': 1, "Yaa": 1, "Safo": 1, "Nana": 1, "Edwin": 1}
+
+
+def test_count_single_item_recurring_multiple_times() -> None:
+    """Tests the situation where the input list contains one item that recurs five times."""
+    collection_to_count: list[str] = ["Ann", "Ann", "Ann", "Ann", "Ann"]
+    assert count(collection_to_count) == {'Ann': 5}
+
+
+def test_count_mix_of_multiple_and_single_recurrences() -> None:
+    """Tests the situation where the input list contains some items that occur once and others that recur multiple times."""
+    collection_to_count: list[str] = ["Ann", "Yaa", "Safo", "Nana", "Safo", "Edwin", "Safo", "Esi", "Nana", "Tetteh"]
+    assert count(collection_to_count) == {'Ann': 1, 'Yaa': 1, 'Safo': 3, 'Nana': 2, 'Edwin': 1, 'Esi': 1, 'Tetteh': 1}
+
+
+def test_count_multiple_items_recurring_multiple_times() -> None:
+    """Tests the situation where the input list contains multiple items that recur multiple times."""
+    collection_to_count: list[str] = ["Ann", "Nana", "Safo", "Ann", "Ann", "Safo", "Safo", "Nana", "Ann", "Safo", "Nana", "Nana", "Safo"]
+    assert count(collection_to_count) == {'Ann': 4, 'Nana': 4, 'Safo': 5}
+
+
+"""Testing the favorite_color function."""
+
+
+def test_favorite_color_single_person_single_color() -> None:
+    """Tests the situation where the input dictionary comprises one name and one color."""
+    name_and_color_tracker: dict[str, str] = {'Ann': 'pink'}
+    assert favorite_color(name_and_color_tracker) == 'pink'
+
+
+def test_favorite_color_multiple_people_like_a_single_color() -> None:
+    """Tests the situation where the input dictionary comprises many people who like one color."""
+    name_and_color_tracker: dict[str, str] = {'Ann': 'pink', 'Yaa': 'pink', 'Safo': 'pink'}
+    assert favorite_color(name_and_color_tracker) == 'pink'
+
+
+def test_favorite_color_one_color_appears_multiple_times() -> None:
+    """Tests the situation where the input dictionary has one color liked by multiple people."""
+    name_and_color_tracker: dict[str, str] = {'Marc': 'yellow', 'Ezri': 'blue', 'Kris': 'blue'}
+    assert favorite_color(name_and_color_tracker) == 'blue'
+
+
+def test_favorite_color_frequencies_of_different_colors_are_equal() -> None:
+    """Tests the situation where there is a tie for most popular color and a correctly implemented function should return the color that appeared in the dictionary first."""
+    name_and_color_tracker: dict[str, str] = {'Ann': 'pink', 'Yaa': 'pink', 'Marc': 'yellow', 'Ezri': 'blue', 'Kris': 'blue'}
+    assert favorite_color(name_and_color_tracker) == 'pink'
